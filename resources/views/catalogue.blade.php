fcaf6dda709654cb4677e4f9c172a9b7634e6277
Helios Martin <heliosmartin123@gmail.com>
2024-05-24 08:49:40 +0200

    v1 books titles catalogue

--- resources/views/catalogue.blade.php
+++ resources/views/catalogue.blade.php
@@ -5,13 +5,15 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Catalogue</title>
+    <link rel="stylesheet" href="./style/catalogue.css">
     <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
     <script src="./js/getAllBooks.js"></script>
 </head>
 
 <body>
     <div x-data="books" x-init="fetchBooks">
-        <h1>Catalogue de Livres</h1>
+        <h1 class="page-title">Catalogue</h1>
+        <div class="title-underline"></div>
         <ul>
             <template x-for="book in books">
                 <div>
