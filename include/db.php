00a8399a2a6cba804f48c5f6ae99fdbf3dc2088f
Elyesse Kourdourli <elyesse@bigsool.com>
2024-05-24 11:09:43 +0200

    Include function to connect to the database

--- include/db.php
+++ include/db.php
@@ -1,14 +1,18 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "root";
-$dbname = "iportefolio";
 
+function Connexion() {
+    $hostname = '185.98.131.149';
+    $username = 'kourd2142291';
+    $password = 'c6qkzhj0sb';
+    $dbname = 'kourd2142291_1u9fc1';
+    $dsn = "mysql:host=$hostname;dbname=$dbname;charset=utf8mb4";
 
-$db = mysqli_connect($servername, $username, $password, $dbname);
-
-// if ($db) {
-//     echo "database is connected !";
-// } else {
-//     echo "something is wrong with database !";
-// }
+    try {
+        $bdd = new PDO($dsn, $username, $password);
+        $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        return $bdd;
+    } catch (PDOException $e) {
+        exit("Erreur de connection: " . $e->getMessage());
+    }
+}
+ ?>
