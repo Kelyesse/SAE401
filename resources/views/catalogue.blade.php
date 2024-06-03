56736444f12758a25b96d4d291e405e985625157
Helios Martin <heliosmartin123@gmail.com>
2024-06-03 20:04:43 +0200

    catalogue html + css

--- resources/views/catalogue.blade.php
+++ resources/views/catalogue.blade.php
@@ -11,20 +11,30 @@
 </head>
 
 <body>
-    <div x-data="books" x-init="fetchBooks">
+    <main class="container">
         <h1 class="page-title">Catalogue</h1>
         <div class="title-underline"></div>
-        <ul>
-            <template x-for="book in books">
-                <div>
-                    <div x-text="book.titre"></div>
-                    <div x-text="book.annee"></div>
-                    <img :src="`{{ asset('storage') }}/${book . imgUrl}`" alt="Book Image">
-                </div>
-            </template>
-        </ul>
-    </div>
 
+        <!-- Search Bar -->
+        <div class="search-bar">
+            <input type="text" placeholder="Rechercher...">
+            <button><img src="./images/search-icon.png" alt="Search"></button>
+            <button><img src="./images/filter-icon.png" alt="Filter"></button>
+        </div>
+
+        <!-- Book List -->
+        <div class="book-list" x-data="books" x-init="fetchBooks">
+            <template x-for="book in books">
+                <template x-if="book.imgUrl">
+                    <div class="book-item">
+                        <img :src="'./storage/' + book . imgUrl" alt="Image du livre">
+                        <div class="book-title" x-text="book.titre"></div>
+                        <div class="book-year" x-text="book.annee"></div>
+                    </div>
+                </template>
+            </template>
+        </div>
+    </main>
 </body>
 
 </html>
