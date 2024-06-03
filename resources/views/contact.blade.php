0126c2ffb229b7f41647f63d7d67e85b3957d251
nisito <joris.dgrt@gmail.com>
2024-06-03 19:40:44 +0200

    upgrade contact

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,8 +1,4 @@
-<?php
-
-include_once('./navbar.blade.php');
-
-?>
+<?php include_once('./navbar.blade.php'); ?>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -11,43 +7,51 @@ include_once('./navbar.blade.php');
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../css/contact.css">
     <title>Contact</title>
+    <script src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js" defer></script>
+    <script src="./js/contact.js" defer></script> <!-- Inclure le fichier JS externe -->
 </head>
-<body>
+<body x-data="contactForm()">
     <div id="titre">
         <h2 id="titre-texte">Formulaire de contact</h2>
         <div id="ligne-rouge"></div>
     </div>
 
-    <form id="section-formulaire">
+    <form id="section-formulaire" @submit.prevent="submitForm">
         <p id="description">Polymedia est à votre service ! <br> Contactez nous pour toute demande d'information ou d'aide</p>
         <section id="champs">
             <div id="nom">
                 <p id="titre-nom" class="titre">Nom</p>
                 <div id="inputs-nom" class="inputs">
-                    <input id="input-prenom" class="input-half" type="text" placeholder="Prénom">
-                    <input id="input-nom" class="input-half" type="text" placeholder="Nom">
+                    <input id="input-prenom" class="input-half" type="text" placeholder="Prénom" x-model="prenom">
+                    <input id="input-nom" class="input-half" type="text" placeholder="Nom" x-model="nom">
                 </div>
             </div>
             <div id="adresse">
                 <p id="titre-adresse" class="titre">Adresse</p>
                 <div id="inputs-adresse" class="inputs">
-                    <input type="text" id="input-adresse" class="input-full" placeholder="Adresse">
+                    <input type="text" id="input-adresse" class="input-full" placeholder="Adresse" x-model="adresse">
                     <div id="sous-adresse">
-                        <input type="text" id="input-ville" class="input-tier" placeholder="Ville">
-                        <input type="text" id="input-codepostal" class="input-tier" placeholder="Code postal">
-                        <input type="text" id="input-complement" class="input-tier" placeholder="Complément">
+                        <input type="text" id="input-ville" class="input-tier" placeholder="Ville" x-model="ville">
+                        <input type="text" id="input-codepostal" class="input-tier" placeholder="Code postal" x-model="codepostal">
+                        <input type="text" id="input-complement" class="input-tier" placeholder="Complément" x-model="complement">
                     </div>
                 </div>
             </div>
             <div id="contact">
                 <p id="titre-contact" class="titre">Contact</p>
                 <div id="inputs-contact" class="inputs">
-                    <input type="email" class="input-half" name="email" id="input-email" placeholder="Email">
-                    <input type="number" class="input-half" id="input-telephone" name="telephone" placeholder="Téléphone">
+                    <input type="email" class="input-half" name="email" id="input-email" placeholder="Email" x-model="email">
+                    <input type="number" class="input-half" id="input-telephone" name="telephone" placeholder="Téléphone" x-model="telephone">
+                </div>
+            </div>
+            <div id="textbox">
+                <p id="titre-contact" class="titre">Question</p>
+                <div id="inputs-question" class="inputs">
+                    <textarea class="input-full" name="question" id="input-question" placeholder="Ecrivez votre question ici" x-model="question"></textarea>
                 </div>
             </div>
             <div id="newsletter">
-                <input type="checkbox" name="news-letter" id="newsletter">
+                <input type="checkbox" name="news-letter" id="newsletter" x-model="newsletter">
                 <p id="text-newsletter">Me tenir informé des actualités et des nouveautés par email</p>
             </div>
         </section>
@@ -56,8 +60,4 @@ include_once('./navbar.blade.php');
 </body>
 </html>
 
-<?php
-
-    include_once('footer.blade.php');
-
-?>
+<?php include_once('footer.blade.php'); ?>
